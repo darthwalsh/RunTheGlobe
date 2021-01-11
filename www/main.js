aad4b2c5604467d9c14f0f56be309516d27ab979
Carl Walsh <darthwalsh@gmail.com>
2021-01-10 19:53:54 -0800

    create www

---
+++ www/main.js
@@ -0,0 +1,18 @@
+"use strict";
+
+function $(id) {
+  return document.getElementById(id);
+}
+/**
+ * @param {HTMLElement} parent
+ * @param {string} name
+ * @param {Object.<string, string>} attributes
+ */
+function create(parent, name, attributes = {}) {
+  const node = document.createElement(name);
+  parent.appendChild(node);
+  for (const prop in attributes) {
+    node.setAttribute(prop, attributes[prop]);
+  }
+  return node;
+}
