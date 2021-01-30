b119fba949f4cae7538143957f402c9fe983abfa
Carl Walsh <darthwalsh@gmail.com>
2021-01-30 09:13:45 -0800

    add opencyclemap and strava overlay

--- www/main.js
+++ www/main.js
@@ -16,3 +16,31 @@ function create(parent, name, attributes = {}) {
   }
   return node;
 }
+
+const map = L.map("mapid");
+
+L.tileLayer("https://{s}.tile.thunderforest.com/cycle/{z}/{x}/{y}.png?apikey={apikey}", {
+  attribution:
+    '&copy; <a href="http://www.thunderforest.com/">Thunderforest</a>, &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  apikey: "0f3c9240e36c48ce9085a96d693d6ab6",
+  maxZoom: 22,
+}).addTo(map);
+
+map.locate({setView: true, maxZoom: 16});
+
+L.control.scale().addTo(map);
+
+const cookies = "?Key-Pair-Id=TODO&Policy=TODO&Signature=TODO";
+// TODO prompt for this and store in localstorage, reprompt after 24 hours; v2 prompt on error
+
+const heatmapUrl =
+  "https://heatmap-external-{s}.strava.com/tiles-auth/run/bluered/{z}/{x}/{y}.png" +
+  cookies;
+
+const strava = L.tileLayer(heatmapUrl, {
+  maxNativeZoom: 15,
+  maxZoom: 22,
+});
+strava.addTo(map);
+
+// L.control.layers(null, {strava}).addTo(map);
