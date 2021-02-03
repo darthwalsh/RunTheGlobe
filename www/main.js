c0e8c70b52779b026eabf1f1a94510c617fa1944
Carl Walsh <darthwalsh@gmail.com>
2021-02-03 10:09:49 -0800

    use JS to prompt and store for cookie

--- www/main.js
+++ www/main.js
@@ -4,17 +4,84 @@ function $(id) {
   return document.getElementById(id);
 }
 /**
- * @param {HTMLElement} parent
+ * @param {HTMLElement | string} parent
  * @param {string} name
  * @param {Object.<string, string>} attributes
  */
 function create(parent, name, attributes = {}) {
   const node = document.createElement(name);
-  parent.appendChild(node);
+  if (typeof parent === "string") {
+    $(parent).appendChild(node);
+  } else {
+    parent.appendChild(node);
+  }
   for (const prop in attributes) {
     node.setAttribute(prop, attributes[prop]);
   }
   return node;
+}
+
+const cookieKey = "STRAVA_COOKIE_KEY";
+function getStoredCookie() {
+  let stored = localStorage.getItem(cookieKey);
+  if (!stored) return;
+
+  const [last, cookie, extra] = stored.split(":");
+  if (!last || !cookie || !Number(last) || extra) return;
+
+  const lastweek = Date.now() - 7 * 24 * 60 * 60 * 1000;
+  if (Number(last) < lastweek) return;
+
+  return cookie;
+}
+
+async function getCookieQuery() {
+  let cookieQuery = getStoredCookie();
+  if (!cookieQuery) {
+    /** @type {HTMLDialogElement} */
+    const dialog = create(document.body, "dialog");
+    dialog.append("Go to ");
+    const a = create(
+      dialog,
+      "a",
+      {href: "https://www.strava.com/heatmap", target: "_blank"}
+    );
+    a.textContent = "strava.com/heatmap";
+    dialog.append(
+      ", open browser devtools, capture a PNG network request, find Cookie request header, paste below:"
+    );
+    create(dialog, "br");
+
+    /** @type {HTMLInputElement} */
+    const input = create(dialog, "input");
+    input.placeholder = "ajs_user_id=...";
+
+    const button = create(dialog, "button");
+    button.textContent = "Submit";
+
+    const promise = new Promise(res => {
+      button.onclick = _ => {
+        dialog.close();
+        res(input.value);
+      };
+    });
+
+    dialog.showModal();
+    /** @type {string} */
+    const cookie = await promise;
+
+    const withoutColon = cookie.split(": ").pop();
+
+    const prefix = "CloudFront-";
+    cookieQuery = withoutColon
+      .split("; ")
+      .filter(s => s.startsWith(prefix))
+      .map(s => s.substr(prefix.length))
+      .join("&");
+
+    localStorage.setItem(cookieKey, `${Date.now()}:${cookieQuery}`);
+  }
+  return cookieQuery;
 }
 
 const map = L.map("mapid");
@@ -30,17 +97,19 @@ map.locate({setView: true, maxZoom: 16});
 
 L.control.scale().addTo(map);
 
-const cookies = "?Key-Pair-Id=TODO&Policy=TODO&Signature=TODO";
-// TODO prompt for this and store in localstorage, reprompt after 24 hours; v2 prompt on error
+async function addGlobal() {
+  const cookieQuery = await getCookieQuery();
+  // MAYBE prompt on API request error?
+  // MAYBE cache cookie/tiles(?) on browser/remotely(?)
 
-const heatmapUrl =
-  "https://heatmap-external-{s}.strava.com/tiles-auth/run/bluered/{z}/{x}/{y}.png" +
-  cookies;
+  const heatmapUrl =
+    "https://heatmap-external-{s}.strava.com/tiles-auth/run/bluered/{z}/{x}/{y}.png?" + cookieQuery;
 
-const strava = L.tileLayer(heatmapUrl, {
-  maxNativeZoom: 15,
-  maxZoom: 22,
-});
-strava.addTo(map);
-
-// L.control.layers(null, {strava}).addTo(map);
+  const strava = L.tileLayer(heatmapUrl, {
+    maxNativeZoom: 15,
+    maxZoom: 22,
+  });
+  strava.addTo(map);
+}
+addGlobal();
+// MAYBE add strava as an optional layer, wait to prompt?
