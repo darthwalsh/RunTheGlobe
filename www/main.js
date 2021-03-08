ee66b8a8c30e033efc967b317c23a646f92b2942
Carl Walsh <darthwalsh@gmail.com>
2021-03-08 09:27:23 -0800

    hardcode some APIs for dev environment
    skip GPS locate prompt
    use url query ?PROD_ENV to skip that

--- www/main.js
+++ www/main.js
@@ -1,5 +1,14 @@
 "use strict";
 
+const urlQuery = new URLSearchParams(window.location.search);
+const DEV_ENV = ["localhost", "127.0.0.1", ""].includes(window.location.hostname) && !urlQuery.has("PROD_ENV");
+if (DEV_ENV) {
+  const favicon = document.querySelector("link[rel~='icon']");
+  if (favicon) {
+    favicon.href = "data:image/svg+xml,%3Csvg%20xmlns='http://www.w3.org/2000/svg'%20viewBox='0%200%2016%2016'%3E%3Ctext%20x='0'%20y='14'%3E🚀%3C/text%3E%3C/svg%3E";
+  }
+}
+
 function $(id) {
   return document.getElementById(id);
 }
@@ -93,7 +102,11 @@ L.tileLayer("https://{s}.tile.thunderforest.com/cycle/{z}/{x}/{y}.png?apikey={ap
   maxZoom: 22,
 }).addTo(map);
 
-map.locate({setView: true, maxZoom: 16});
+if (DEV_ENV) {
+  map.setView({lon: -122.53, lat: 38.03}, 16);
+} else {
+  map.locate({setView: true, maxZoom: 16});
+}
 
 L.control.scale().addTo(map);
 
