ffa5af488fed58f3cea676088e5c0d002a419f15
Carl Walsh <darthwalsh@gmail.com>
2024-07-15 22:33:59 -0700

    Force gen1
    See email text in https://github.com/firebase/extensions/issues/2127

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,5 @@
 const admin = require("firebase-admin");
-const functions = require("firebase-functions");
+const functions = require("firebase-functions/v1");
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
